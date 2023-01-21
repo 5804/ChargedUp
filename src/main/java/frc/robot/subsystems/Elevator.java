4088b2f9042a6e3af1756e3145b319cd0646abde
Taylor McCorkill <taylor.mccorkill@gmail.com>
2023-01-21 10:42:07 -0500

    update code

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems;
 
-import javax.swing.text.Position;
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.SensorCollection;
@@ -13,7 +11,6 @@ import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
@@ -21,72 +18,66 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import javax.swing.text.Position;
 
 public class Elevator extends SubsystemBase {
 
-    public TalonFX mainMotor;
-    public TalonFX followerMotor;
-    public double targetVelocity;
+  public TalonFX mainMotor;
+  public TalonFX followerMotor;
+  public double targetVelocity;
 
-    public Elevator() {
-        mainMotor = new TalonFX(1, "torch"); // change
-        followerMotor = new TalonFX(2, "torch"); // change
+  public Elevator() {
+    mainMotor = new TalonFX(1, "torch"); // change
+    followerMotor = new TalonFX(2, "torch"); // change
 
-        mainMotor.configFactoryDefault();
-        followerMotor.configFactoryDefault();
-        // 491,717
-        //mainMotor.configForwardSoftLimitThreshold(3.0);
-        mainMotor.setNeutralMode(NeutralMode.Brake);
-        followerMotor.setNeutralMode(NeutralMode.Brake);
-        followerMotor.follow(mainMotor);
-        followerMotor.setInverted(TalonFXInvertType.Clockwise); // subject to change
-        mainMotor.setInverted(TalonFXInvertType.CounterClockwise);
+    mainMotor.configFactoryDefault();
+    followerMotor.configFactoryDefault();
+    // 491,717
+    //mainMotor.configForwardSoftLimitThreshold(3.0);
+    mainMotor.setNeutralMode(NeutralMode.Brake);
+    followerMotor.setNeutralMode(NeutralMode.Brake);
+    followerMotor.follow(mainMotor);
+    followerMotor.setInverted(TalonFXInvertType.Clockwise); // subject to change
+    mainMotor.setInverted(TalonFXInvertType.CounterClockwise);
 
-        mainMotor.configNeutralDeadband(0.01);
+    mainMotor.configNeutralDeadband(0.01);
+  }
 
-    }
+  // public void runUp(){
+  //     mainMotor.set(TalonFXControlMode.PercentOutput, .5);
+  // }
 
-    // public void runUp(){
-    //     mainMotor.set(TalonFXControlMode.PercentOutput, .5);
-    // } 
+  //public CommandBase runUp(){
+  //    return run(() -> mainMotor.set(TalonFXControlMode.PercentOutput, .5))
+  //            .finallyDo(interrupted -> mainMotor.set(ControlMode.PercentOutput, 0.0))
+  //            .withName("runUp");
+  //}
 
-    //public CommandBase runUp(){
-    //    return run(() -> mainMotor.set(TalonFXControlMode.PercentOutput, .5))
-    //            .finallyDo(interrupted -> mainMotor.set(ControlMode.PercentOutput, 0.0))
-    //            .withName("runUp");
-    //}
+  //public CommandBase runDown(){
+  //    return run(() -> mainMotor.set(TalonFXControlMode.PercentOutput, -.5))
+  //            .finallyDo(interrupted -> mainMotor.set(ControlMode.PercentOutput, 0.0))
+  //            .withName("runDown");
+  //}
 
-    //public CommandBase runDown(){
-    //    return run(() -> mainMotor.set(TalonFXControlMode.PercentOutput, -.5))
-    //            .finallyDo(interrupted -> mainMotor.set(ControlMode.PercentOutput, 0.0))
-    //            .withName("runDown");
-    //}
-    
-    public void runUp(){
-        mainMotor.set(TalonFXControlMode.PercentOutput, -0.3);
-    }
+  public void runUp() {
+    mainMotor.set(TalonFXControlMode.PercentOutput, -0.3);
+  }
 
-    public void runDown(){
-        mainMotor.set(TalonFXControlMode.PercentOutput, 0.3);
-    }
+  public void runDown() {
+    mainMotor.set(TalonFXControlMode.PercentOutput, 0.3);
+  }
 
-    public void elevatorStop(){
-        mainMotor.set(TalonFXControlMode.PercentOutput, 0.0);
-    }
+  public void elevatorStop() {
+    mainMotor.set(TalonFXControlMode.PercentOutput, 0.0);
+  }
 
+  public void reset() {
+    mainMotor.setSelectedSensorPosition(0);
+  }
 
-    public void reset(){
-        mainMotor.setSelectedSensorPosition(0);
-    }
-
-
-
-
-    @Override
-    public void periodic(){
-
-        double position = mainMotor.getSelectedSensorPosition();
-        SmartDashboard.putNumber("sensor position", position);
-
-    }
+  @Override
+  public void periodic() {
+    double position = mainMotor.getSelectedSensorPosition();
+    SmartDashboard.putNumber("sensor position", position);
+  }
 }
